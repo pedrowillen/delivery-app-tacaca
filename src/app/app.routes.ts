e35b33cc1fcf0846efc02c0e80f7fd83f555d5f4
Pedro Willen <pedrowillen28@gmail.com>
2024-09-19 00:23:50 -0400

    adding delivery page component and setting routes of them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DeliveryPageComponent } from './components/delivery-page/delivery-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'delivery', component: DeliveryPageComponent}
+];
--- src/app/components/delivery-page/delivery-page.component.ts
+++ src/app/components/delivery-page/delivery-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-delivery-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './delivery-page.component.html',
   styleUrl: './delivery-page.component.css'
 })
 export class DeliveryPageComponent {
-
+  tacacaMenu = [
+    {name: 'Tacacá Médio', stock: '999', price: '25.99'},
+    {name: 'Tacacá Grande', stock: '999', price: '30.99'},
+    {name: 'Combo Dois (2) Tacacá Grande', stock: '999', price: '55.99'},
+    {name: 'Combo Tacacá Médio + Tacacá Grande', stock: '999', price: '50.99'},
+    {name: 'Combo Dois (2) Tacacá Médio', stock: '999', price: '45.99'},
+  ];
+  
 }
