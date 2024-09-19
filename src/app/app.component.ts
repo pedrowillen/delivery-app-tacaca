edbf9a953bcc6aed756160eef2637c8485678dd4
Pedro Willen <pedrowillen28@gmail.com>
2024-09-18 22:54:25 -0400

    adding delivery page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DeliveryPageComponent } from './components/delivery-page/delivery-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, DeliveryPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
